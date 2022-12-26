1a5cfc56e327a7e9faea4d9c0996b0b8e658bea9
HugoSwen <2407475450@qq.com>
2022-12-26 16:36:04 +0800

    12-26

---
+++ data_structure/KMP-1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e5 + 10, M = 1e6 + 10;
+
+int n, m;
+char p[N], s[M];
+int ne[N]; // 存储当前位置前面的最大公共前后缀长度 + 1 (规定ne[1] = 0)
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> p + 1 >> m >> s + 1;
+
+    for (int i = 2, j = 0; i <= n + 1; i++)
+    {
+        while (j && p[i - 1] != p[j])
+            j = ne[j];
+        ne[i] = ++j;
+        j = ne[i];
+    }
+
+    // int i = 1, j = 1;
+    // while (i <= m)
+    // {
+    //     if (j == 0 || s[i] == p[j])
+    //         i++, j++;
+    //     else
+    //         j = ne[j];
+
+    //     if (j == n + 1)
+    //     {
+    //         cout << i - n - 1 << " ";
+    //         j = ne[j];
+    //     }
+    // }
+
+    for (int i = 1, j = 1; i <= m; i++)
+    {
+        while (j && s[i] != p[j])
+            j = ne[j];
+        j++;
+        if (j == n + 1)
+        {
+            cout << i - n << " ";
+            j = ne[j];
+        }
+    }
+
+    return 0;
+}
---
+++ data_structure/KMP-2.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+const int N = 1e7 + 10, M = 1e6 + 10;
+
+int n, m;
+char p[N], s[M];
+int ne[N]; // ne[]存储包括当前位置最大公共前后缀长度
+
+int main()
+{
+    cin >> n >> p + 1 >> m >> s + 1;
+
+    for (int i = 2, j = 0; i <= n; i++)
+    {
+        while (j && p[i] != p[j + 1])
+            j = ne[j];
+        if (p[i] == p[j + 1])
+            j++;
+        ne[i] = j;
+    }
+
+    for (int i = 1, j = 0; i <= m; i++)
+    {
+        while (j && s[i] != p[j + 1])
+            j = ne[j];
+        if (s[i] == p[j + 1])
+            j++;
+        if (j == n)
+        {
+            printf("%d ", i - n);
+            j = ne[j];
+        }
+    }
+
+    return 0;
+}
